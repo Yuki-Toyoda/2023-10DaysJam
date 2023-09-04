fb566d506d7bd5b93370c031f039354574e8bc84
ToyodaYuki <115054848+Yuki-Toyoda@users.noreply.github.com>
2023-09-04 14:46:03 +0900

    数学クラス
    Vector2
    Vector3
    Vector4
    Matrix4x4
    の更新

--- DirectXGame/math/Vector4.h
+++ DirectXGame/math/Vector4.h
@@ -1,11 +1,10 @@
 #pragma once
-
 /// <summary>
-/// 4次元ベクトル
+/// 4次元ベクトル構造体
 /// </summary>
 struct Vector4 final {
-	float x;
-	float y;
-	float z;
-	float w;
+	float x; // x軸
+	float y; // y軸
+	float z; // z軸
+	float w; // w
 };
